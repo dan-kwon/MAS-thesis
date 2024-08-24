cda0693f19865de320eaf7db9d5114fd4e415952
dan-kwon <danielkwon02@gmail.com>
2024-08-24 15:02:10 -0700

    switch to instruct-pix2pix model

--- diffuser.py
+++ diffuser.py
@@ -1,35 +1,27 @@
+import PIL
+import requests
 from torchvision import transforms
 from torchvision.transforms import v2
-from diffusers import StableDiffusionImageVariationPipeline
-from PIL import Image
+import torch
+from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
 
-device = "cpu"
+model_id = "timbrooks/instruct-pix2pix"
+pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
+pipe.to("cpu")
+pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 
-sd_pipe = StableDiffusionImageVariationPipeline.from_pretrained(
-    "lambdalabs/sd-image-variations-diffusers",
-    revision="v2.0",
-    )
-
-sd_pipe = sd_pipe.to(device)
-
-im = Image.open("data/alzheimer_mri/train/MildDemented/MildDemented_0.png")
-
+image = PIL.Image.open("data/alzheimer_mri/train/MildDemented/MildDemented_0.png")
+image = PIL.ImageOps.exif_transpose(image)
+image = image.convert("RGB")
 tform = transforms.Compose([
     transforms.ToTensor(),
     v2.Grayscale(num_output_channels=3),
     transforms.Resize(
-        (224, 224),
-        interpolation=transforms.InterpolationMode.BICUBIC,
-        antialias=False,
-        ),
-    transforms.Normalize(
-        [0.48145466, 0.4578275, 0.40821073],
-        [0.26862954, 0.26130258, 0.27577711]
-        ),
+        (224, 224)),
     ])
 
 inp = tform(im).to(device).unsqueeze(0)
 
-out = sd_pipe(inp, guidance_scale=3)
-
-out["images"][0].save("result.jpg")
+prompt = "create a similar image"
+images = pipe(prompt, image=image, num_inference_steps=5, image_guidance_scale=1.5, text_guidance_scale=7.5).images
+images[0]
