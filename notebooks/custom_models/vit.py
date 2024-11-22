8d6d8c7fc94dc1a340a894bad46021c767e4195a
dan-kwon <danielkwon02@gmail.com>
2024-11-22 02:28:26 +0000

    add vit results

---
+++ notebooks/custom_models/vit.py
@@ -0,0 +1,143 @@
+import torch
+from torchvision.datasets import OxfordIIITPet
+import matplotlib.pyplot as plt
+from random import random
+from torchvision.transforms import Resize, ToTensor
+from torchvision.transforms.functional import to_pil_image
+from torch import nn
+from einops.layers.torch import Rearrange
+from torch import Tensor
+from einops import rearrange, repeat
+
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, image, target):
+        for t in self.transforms:
+            image = t(image)
+        return image, target
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels = 3, patch_size = 8, emb_size = 128):
+        self.patch_size = patch_size
+        super().__init__()
+        self.projection = nn.Sequential(
+            # break-down the image in s1 x s2 patches and flat them
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size),
+            nn.Linear(patch_size * patch_size * in_channels, emb_size)
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.projection(x)
+        return x
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, n_heads, dropout):
+        super().__init__()
+        self.n_heads = n_heads
+        self.att = torch.nn.MultiheadAttention(embed_dim=dim,
+                                               num_heads=n_heads,
+                                               dropout=dropout)
+        self.q = torch.nn.Linear(dim, dim)
+        self.k = torch.nn.Linear(dim, dim)
+        self.v = torch.nn.Linear(dim, dim)
+
+    def forward(self, x):
+        q = self.q(x)
+        k = self.k(x)
+        v = self.v(x)
+        attn_output, attn_output_weights = self.att(x, x, x)
+        return attn_output
+    
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+    
+
+class FeedForward(nn.Sequential):
+    def __init__(self, dim, hidden_dim, dropout = 0.):
+        super().__init__(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+
+class ResidualAdd(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.fn(x, **kwargs)
+        x += res
+        return x
+    
+
+class ViT(nn.Module):
+    def __init__(self, ch=3, img_size=144, patch_size=4, emb_dim=32,
+                n_layers=6, out_dim=37, dropout=0.1, heads=2):
+        super(ViT, self).__init__()
+
+        # Attributes
+        self.channels = ch
+        self.height = img_size
+        self.width = img_size
+        self.patch_size = patch_size
+        self.n_layers = n_layers
+
+        # Patching
+        self.patch_embedding = PatchEmbedding(in_channels=ch,
+                                              patch_size=patch_size,
+                                              emb_size=emb_dim)
+        # Learnable params
+        num_patches = (img_size // patch_size) ** 2
+        self.pos_embedding = nn.Parameter(
+            torch.randn(1, num_patches + 1, emb_dim))
+        self.cls_token = nn.Parameter(torch.rand(1, 1, emb_dim))
+
+        # Transformer Encoder
+        self.layers = nn.ModuleList([])
+        for _ in range(n_layers):
+            transformer_block = nn.Sequential(
+                ResidualAdd(PreNorm(emb_dim, Attention(emb_dim, n_heads = heads, dropout = dropout))),
+                ResidualAdd(PreNorm(emb_dim, FeedForward(emb_dim, emb_dim, dropout = dropout))))
+            self.layers.append(transformer_block)
+
+        # Classification head
+        self.head = nn.Sequential(nn.LayerNorm(emb_dim), nn.Linear(emb_dim, out_dim))
+
+
+    def forward(self, img):
+        # Get patch embedding vectors
+        x = self.patch_embedding(img)
+        b, n, _ = x.shape
+
+        # Add cls token to inputs
+        cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+        x = torch.cat([cls_tokens, x], dim=1)
+        x += self.pos_embedding[:, :(n + 1)]
+
+        # Transformer layers
+        for i in range(self.n_layers):
+            x = self.layers[i](x)
+
+        # Output based on classification token
+        return self.head(x[:, 0, :])
+    
+if __name__=='__main__':
+    model = ViT()
+    print(model)
+    model(torch.ones((1, 3, 144, 144)))
